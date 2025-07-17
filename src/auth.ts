8ecaf8f941ea7dd269e280633c15f77771de3611
Nico Baier <nico.baier@gmail.com>
2025-07-17 01:06:49 -0500

    initial commit

---
+++ src/auth.ts
@@ -0,0 +1,36 @@
+import { betterAuth } from "better-auth";
+import { twoFactor } from "better-auth/plugins";
+import { passkey } from "better-auth/plugins/passkey";
+import Database from "better-sqlite3";
+
+export const auth = betterAuth({
+	database: new Database("./db.sqlite"),
+	account: {
+		accountLinking: {
+			enabled: true,
+			trustedProviders: ["google"],
+		},
+	},
+	emailAndPassword: {
+		enabled: true,
+	},
+	plugins: [
+		passkey(),
+		twoFactor({
+			otpOptions: {
+				async sendOTP(user, otp) {
+					console.log(`Sending OTP to ${user.email}: ${otp}`);
+					// await resend.emails.send({
+					// 	from: "Acme <no-reply@demo.better-auth.com>",
+					// 	to: user.email,
+					// 	subject: "Your OTP",
+					// 	html: `Your OTP is ${otp}`,
+					// });
+				},
+			},
+		}),
+	],
+	rateLimit: {
+		enabled: true,
+	},
+});
